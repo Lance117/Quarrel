bafb5c154237102cba42200c6320fce1ea95b2eb
Lance Wong <lwong180@gmail.com>
2019-11-12 00:00:43 -0800

    Little additions to message model and controller

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,13 @@
 class Api::MessagesController < ApplicationController
-    before_action :require_signed_in, only: [:create]
+    before_action :require_signed_in
 
     def create
         
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:body)
     end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ApplicationRecord
     belongs_to :user
     belongs_to :channel
     validates :body, presence: true
+    scope :for_display, -> { order(:created_at).last(42) }
 end
