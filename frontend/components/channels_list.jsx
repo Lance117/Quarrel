48732b5a379cb0cc7d89b7ddaeffc686f5082991
Lance Wong <lwong180@gmail.com>
2019-12-02 23:30:40 -0800

    Add channel modal description and styling

--- frontend/components/channels_list.jsx
+++ frontend/components/channels_list.jsx
@@ -62,20 +62,50 @@ class ChannelsList extends React.Component {
                         </div>
                     </div>
                     <div className="add-channel-content">
+                        <div className="add-channel-description">
+                            Channels are the battlegrounds for discussions and arguments.
+                            They're best when organized around a topic — #ramen, for example.
+                        </div>
+                        <br></br>
                         <div className="content-section" style={{width: '100%'}}>
-                            <form>
-                                <label><strong>Name</strong></label>
-                                <div className="channel-name-input">
-                                    <input type="text" className="add-channel-input" minLength="3" maxLength="42" placeholder="# e.g. Kpop"/>
-                                </div>
-                                <div className="create-channel-footer">
-                                    <button className="create-channel-btn">Create</button>
-                                </div>
-                            </form>
+                            <AddChannelForm /> 
                         </div>
                     </div>
                 </ReactModal>
             </div>
+        )
+    }
+}
+
+class AddChannelForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value});
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+    }
+
+    render() {
+        let btnClass = this.state.value.length === 0 ? "btn-disabled" : "btn-enabled";
+        return (
+            <form>
+                <label><strong>Name</strong></label>
+                <div className="channel-name-input">
+                    <input type="text" className="add-channel-input" minLength="3" maxLength="42" placeholder="# e.g. Kpop" onChange={this.handleChange} />
+                </div>
+                <div className="create-channel-footer">
+                    <button className={btnClass}>Create</button>
+                </div>
+            </form> 
         )
     }
 }
