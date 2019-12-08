8c13dba53a470e68293747c3d4c8f1ddc04830b1
Lance Wong <lwong180@gmail.com>
2019-12-08 01:49:45 -0800

    message area overflow scroll, footer persistent in bottom of viewport

--- frontend/components/primary_view.jsx
+++ frontend/components/primary_view.jsx
@@ -10,24 +10,29 @@ class PrimaryView extends React.Component {
     render() {
         return (
             <div className="workspace-primary">
-                <div className="msg-forward" style={{top: "0px"}}>
-                    <h1 className="forward-channel-name">
-                        <span>{`#${this.props.channels[this.props.activeChannel.id].channel_name}`}</span>
-                    </h1>
-                    <p className="forward-description">
-                        This is the very beginning of the <strong>#{this.props.channels[this.props.activeChannel.id].channel_name}</strong> channel.
-                        Please stay on topic!
-                    </p>
-                </div>
                 <div className="primary-contents">
-                    {Object.values(this.props.messages).map((message, i) => {
-                        if (message.channel_id === this.props.activeChannel.id) {
-                            return (<Message key={message.id} body={message.body} user={this.props.users[message.user_id]} timestamp={message.created_at}/>)
-                        }
-                    })}
+                    <div className="primary-body">
+                        <div className="msg-pane">
+                            <div className="msg-forward" style={{top: "0px"}}>
+                                <h1 className="forward-channel-name">
+                                    <span>{`#${this.props.channels[this.props.activeChannel.id].channel_name}`}</span>
+                                </h1>
+                                <p className="forward-description">
+                                    This is the very beginning of the <strong>#{this.props.channels[this.props.activeChannel.id].channel_name}</strong> channel.
+                                    Please stay on topic!
+                                </p>
+                            </div>
+                            {Object.values(this.props.messages).map((message, i) => {
+                                if (message.channel_id === this.props.activeChannel.id) {
+                                    return (<Message key={message.id} body={message.body} user={this.props.users[message.user_id]} timestamp={message.created_at}/>)
+                                }
+                            })}
+                    </div>
+
+                        </div>
+                    <PrimaryFooter currentUser={this.props.currentUser} channelId={this.props.activeChannel.id} activeChannelName={this.props.channels[this.props.activeChannel.id].channel_name} 
+                    createMessage={this.props.createMessage}/>
                 </div>
-                <PrimaryFooter currentUser={this.props.currentUser} channelId={this.props.activeChannel.id} activeChannelName={this.props.channels[this.props.activeChannel.id].channel_name} 
-                createMessage={this.props.createMessage}/>
             </div>
         )
     }
