6c298c4b1b06796d30ef9eee9325b81173b936d6
Lance Wong <lwong180@gmail.com>
2019-12-27 00:23:01 -0800

    default avatars-use hash fn on username to assign

--- frontend/components/message.jsx
+++ frontend/components/message.jsx
@@ -3,9 +3,26 @@ import ReactModal from 'react-modal';
 import Moment from 'react-moment'
 import validator from 'validator'
 import YouTube from 'react-youtube'
+import stringHash from 'string-hash'
 
 function Message(props) {
     const [showmodal, setShowmodal] = useState([false, null, null]);
+    const avatars = [
+        'https://ca.slack-edge.com/T03GU501J-UJZAX6WDU-g5e90ac62e9f-72',
+        'https://ca.slack-edge.com/T03GU501J-UL2N3LG2X-gf1556aa48ff-72',
+        'https://ca.slack-edge.com/T03GU501J-ULUDZ1QKS-g7b70ea0cbae-72',
+        'https://ca.slack-edge.com/T03GU501J-UKT0C8SS1-g665db23aba0-72',
+        'https://ca.slack-edge.com/T03GU501J-UMHG8Q2NA-g7eee62267eb-72',
+        'https://ca.slack-edge.com/T03GU501J-ULNGNK0LC-g5f57353e5f4-72',
+        'https://ca.slack-edge.com/T03GU501J-UBVJX8CB1-g59c98ec02a0-48',
+        'https://ca.slack-edge.com/T03GU501J-UL4FRHEER-gebe3e59ff06-72',
+        'https://ca.slack-edge.com/T03GU501J-ULXV025EG-g9d24e4c4d3d-72',
+        'https://ca.slack-edge.com/T03GU501J-UDEMKU2KD-g99e9e862d66-72',
+        'https://ca.slack-edge.com/T03GU501J-UPEMSDDF1-g642684ec089-72',
+        'https://ca.slack-edge.com/T03GU501J-ULG8PH0F8-g1ff632e59b1-72',
+        'https://ca.slack-edge.com/T03GU501J-UM3KRAGQ2-g2ab2756ba7a-72',
+        'https://ca.slack-edge.com/T03GU501J-U4PP3VBR8-gb7de446f194-72'
+    ]
     let msgBody = createMsgBody(props.body);
     let className = "msg-list-item";
     if (props.lastMsg) className = className.concat(' last-msg');
@@ -32,7 +49,7 @@ function Message(props) {
         <div className={className}>
             <div className="msg-avatar">
                 <button className="msg-avatar-btn">
-                    <img className="avatar-img" src="https://ca.slack-edge.com/T03GU501J-UBVJX8CB1-g59c98ec02a0-48" />
+                    <img className="avatar-img" src={avatars[parseInt(stringHash(props.user.username))%avatars.length]} />
                 </button>
             </div>
             <div className="msg-contents">
