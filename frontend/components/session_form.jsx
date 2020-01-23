e196b8da11ae5f1bf52f8c7a59e469af15e477cf
Lance Wong <lwong180@gmail.com>
2020-01-22 23:32:02 -0800

    guest login button in session form navbar

--- frontend/components/session_form.jsx
+++ frontend/components/session_form.jsx
@@ -60,7 +60,7 @@ class SessionForm extends React.Component {
     render() {
         return (
             <div className="formpage">
-                <SessionTop />
+                <SessionTop processForm={this.props.processForm}/>
                 <div className="page-contents">
                     {this.renderErrors()}
                     <div className="content-card span-2-3 col">
--- frontend/components/session_top.jsx
+++ frontend/components/session_top.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import {NavLink} from 'react-router-dom';
 
-const SessionTop = () => (
-    <nav className="top-persistent">
-        <NavLink to="/" className="formlogo"></NavLink> 
-       <ul className="form-navlist">
-           <li className="sign-in">
-               <NavLink to="/signin" className="signin-btn">Sign in</NavLink>
-           </li>
-       </ul>
-    </nav>
-)
+function SessionTop(props) {
+    const guest = {
+        email: "karatekid@gmail.com",
+        password: "karatekid"
+    };
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        const user = Object.assign({}, guest);
+        props.processForm(user);
+    }
+
+    return (
+        <nav className="top-persistent">
+            <NavLink to="/" className="formlogo"></NavLink> 
+        <ul className="home-navlist">
+            <li className="home-nav-item home-hover">
+                <form onSubmit={handleSubmit}>
+                    <button className="guest-btn">Guest login</button>
+                </form>
+            </li>
+            <li className="home-nav-item home-hover sign-in">
+                <NavLink to="/signin" className="signin-btn">Sign in</NavLink>
+            </li>
+        </ul>
+        </nav>
+    )
+}
 
 export default SessionTop;
